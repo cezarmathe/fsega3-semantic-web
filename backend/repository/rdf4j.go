9ee170544e678d8b0bee11c19aca0dd4cf14aa85
Armand-Cezar Mathe <me@cezarmathe.com>
2023-05-17 21:45:56 +0300

    Add rdf4j client

--- backend/repository/rdf4j.go
+++ backend/repository/rdf4j.go
@@ -2,18 +2,32 @@ package repository
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 
 	"github.com/cezarmathe/semweb/backend/types"
 )
 
+func must[T any](t T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+
+	return t
+}
+
 type RDF4J struct {
-	base       string
+	base       *url.URL
 	httpClient HTTPClient
 }
 
 func NewRDF4J(base string, httpClient HTTPClient) *RDF4J {
 	return &RDF4J{
-		base:       base,
+		base:       must(url.Parse(base)),
 		httpClient: httpClient,
 	}
 }
@@ -23,32 +37,124 @@ func (r *RDF4J) DeleteOneByURL(ctx context.Context, url string) (types.BlogPost,
 }
 
 func (r *RDF4J) FindAll(ctx context.Context) ([]types.BlogPost, error) {
-	panic("not implemented")
+	endpoint := must(url.Parse(r.base.String()))
+	form := url.Values{}
+	form.Set("query", `
+		select ?subject ?predicate ?object from <http://example.com/articles> where { ?subject ?predicate ?object }
+	`)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint.String(), strings.NewReader(form.Encode()))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(form.Encode())))
+	req.Header.Set("Accept", "application/sparql-results+json")
+
+	resp, err := r.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	var respBody rdfJSONResponse
+	err = json.NewDecoder(resp.Body).Decode(&respBody)
+	if err != nil {
+		return nil, err
+	}
+
+	blogPosts := make(map[string]types.BlogPost, len(respBody.Results.Bindings)/3)
+
+	for _, v := range respBody.Results.Bindings {
+		blogPost := blogPosts[v.Subject.Value]
+
+		blogPost.URL = v.Subject.Value
+
+		switch v.Predicate.Value {
+		case "http://example.com/articleauthor":
+			blogPost.Author = v.Object.Value
+		case "http://example.com/articledate":
+			blogPost.Date = v.Object.Value
+		case "http://example.com/articletitle":
+			blogPost.Title = v.Object.Value
+		}
+
+		blogPosts[v.Subject.Value] = blogPost
+	}
+
+	result := make([]types.BlogPost, 0, len(blogPosts))
+
+	for _, v := range blogPosts {
+		result = append(result, v)
+	}
+
+	return result, nil
 }
 
 func (r *RDF4J) SaveOne(ctx context.Context, blogPost types.BlogPost) (types.BlogPost, error) {
-	panic("not implemented")
+	endpoint := must(url.Parse(r.base.String() + "/statements"))
+	form := url.Values{}
+	form.Set("update", fmt.Sprintf(
+		`
+		prefix articlepredicate: <http://example.com/article>
+
+		insert data {
+			graph <http://example.com/articles> {
+				<%s>
+					articlepredicate:author "%s";
+					articlepredicate:date "%s";
+					articlepredicate:title "%s".
+			}
+		}
+		`,
+		blogPost.URL, blogPost.Author, blogPost.Date, blogPost.Title,
+	))
+	payload := form.Encode()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint.String(), strings.NewReader(payload))
+	if err != nil {
+		return types.BlogPost{}, err
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(payload)))
+
+	resp, err := r.httpClient.Do(req)
+	if err != nil {
+		return types.BlogPost{}, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusNoContent {
+		return types.BlogPost{}, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	return blogPost, nil
 }
 
-// graph: http://example.com
-// <subject> <predicate> <object> format for triples
-// <subject> is the URL
-// <predicate> is the data type (always omit URL!)
-// <object> is the value
+type rdfJSONResponse struct {
+	Head struct {
+		Vars []string `json:"vars"`
+	} `json:"head"`
+	Results struct {
+		Bindings []rdfBindingTriple `json:"bindings"`
+	} `json:"results"`
+}
 
-// INSERTING:
+type rdfBinding struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
 
-// prefix articlepredicate: <http://example.com/article>
-
-// insert data {
-//   graph <http://example.com/articles> {
-//     <https://drewdevault.com/2023/05/01/2023-05-01-Burnout.html>
-//       articlepredicate:author "Drew DeVault";
-//       articlepredicate:date "2023-05-01";
-//       articlepredicate:title "Burnout".
-//   }
-// }
-
-// SELECTING:
-
-// select ?subject ?predicate ?object from <http://example.com/articles> where { ?subject ?predicate ?object }
+type rdfBindingTriple struct {
+	Subject   rdfBinding `json:"subject"`
+	Predicate rdfBinding `json:"predicate"`
+	Object    rdfBinding `json:"object"`
+}
--- backend/repository/rdf4j_test.go
+++ backend/repository/rdf4j_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/cezarmathe/semweb/backend/types"
 )
 
-func TestRDF4J(t *testing.T) {
+func TestRDF4JSaveOne(t *testing.T) {
 	ctx := context.Background()
 
 	client := repository.NewRDF4J(
@@ -18,9 +18,10 @@ func TestRDF4J(t *testing.T) {
 	)
 
 	expected := types.BlogPost{
-		Date:  "1970-01-01",
-		Title: "Hello, world!",
-		URL:   "https://example.com",
+		Author: "Drew DeVault",
+		Date:   "1970-01-01",
+		Title:  "Hello, world!",
+		URL:    "https://example3.com",
 	}
 
 	actual, err := client.SaveOne(ctx, expected)
@@ -30,5 +31,25 @@ func TestRDF4J(t *testing.T) {
 
 	if actual != expected {
 		t.Fatalf("SaveOne failed: expected %v, got %v", expected, actual)
+	}
+}
+
+func TestRDF4JFindAll(t *testing.T) {
+	ctx := context.Background()
+
+	client := repository.NewRDF4J(
+		"http://localhost:8080/rdf4j-server/repositories/grafexamen",
+		http.DefaultClient,
+	)
+
+	expected := []types.BlogPost{}
+
+	actual, err := client.FindAll(ctx)
+	if err != nil {
+		t.Fatalf("FindAll failed: %v", err)
+	}
+
+	if len(actual) != len(expected) {
+		t.Fatalf("FindAll failed: expected %v, got %v", expected, actual)
 	}
 }
